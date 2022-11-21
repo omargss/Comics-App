89f0610458b35887a0dcb2c81badaaeda767bd07
Mathis <mathis.thuel@gmail.com>
2022-11-21 17:59:59 +0100

    Ajout de la classe comic qui permet de mieux récupérer les données, elles seront plus faciles à process par la suite

--- Prinfo/src/main/java/GetData/GetComicsData.java
+++ Prinfo/src/main/java/GetData/GetComicsData.java
@@ -1,14 +1,12 @@
 package GetData;
-
+import Objects.Comic;
 //f6929d31c63612dd656e42295cc122010ac74c1c
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.io.*;
 import java.util.*;
-import org.json.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -17,20 +15,18 @@ import org.json.simple.parser.ParseException;
 public class GetComicsData {
 
 	public static void main(String[] args) {
-		getComicsData("hulk");
+		getComicsData("hulk", "name:asc", "25"); // bizzare, on a 5 fois moins de résultats que prévu... à voir
 
 	}
 
-	public static void getComicsData(String title) {
+	public static void getComicsData(String name, String sort, String limit) {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(
-				"https://comicvine.gamespot.com/api/volumes/?api_key=f6929d31c63612dd656e42295cc122010ac74c1c&format=json&sort=name:asc&filter=name:"
-						+ title))
-				.build();
+		"https://comicvine.gamespot.com/api/volumes/?api_key=f6929d31c63612dd656e42295cc122010ac74c1c&format=json&sort="+sort+"&limit="+limit+"&filter=name:"+ name)).build();
 
 		HttpResponse<String> response;
 		try {
-			List<List<String>> list = new ArrayList<List<String>>();
+			List<Comic> list = new ArrayList<Comic>();
 			response = client.send(request, HttpResponse.BodyHandlers.ofString());
 			JSONParser parser = new JSONParser();
 			JSONObject jsonObject = (JSONObject) parser.parse(response.body());
@@ -39,20 +35,24 @@ public class GetComicsData {
 			Iterator<JSONObject> iterator = results.iterator();
 
 			while (iterator.hasNext()) {
-				// plus tard il faudra mettre sous forme d'une liste d'objets de classe comics, à voir j'ai pas le temps
-				ArrayList<String> listToAdd = new ArrayList<String>();
-				listToAdd.add((String)iterator.next().get("name")); // 0
-				listToAdd.add((String)iterator.next().get("site_detail_url")); // 1
-				listToAdd.add((String)iterator.next().get("date_added"));
-				listToAdd.add((String)iterator.next().get("description"));
+				Comic comic = new Comic();
+				comic.setDate((String)iterator.next().get("date_added"));
+				comic.setName((String)iterator.next().get("name"));
+				comic.setUrl((String)iterator.next().get("site_detail_url"));
+				JSONObject image = (JSONObject)iterator.next().get("image");
+				comic.setImage((String)image.get("original_url"));
+				JSONObject publisher = (JSONObject)iterator.next().get("publisher");
+				comic.setPublisher((String)publisher.get("name"));
 				// Problème sur les publishers, certains comics n'ont pas de publisher, à voir comment faire
-
-
-				list.add(listToAdd);
+				list.add(comic);
 			}
-			for(int j = 0; j < list.size();j++)
-				System.out.println(list.get(j).get(0)) ;
-
+			for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i).getName());
+			System.out.println(list.get(i).getDate());
+			System.out.println(list.get(i).getPublisher());
+			System.out.println(list.get(i).getImage());
+			System.out.println(list.get(i).getUrl());
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ Prinfo/src/main/java/Objects/Comic.java
@@ -0,0 +1,45 @@
+package Objects;
+
+public class Comic {
+	private String name;
+	private String url;
+	private String date;
+	private String image;
+	private String publisher;
+	
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+
+}
